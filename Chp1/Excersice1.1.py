0645b2e4b1ded1243626b179c5bbb6be8b5b4cdc
safiab <safia.hirari@gmail.com>
2018-01-15 14:10:06 +0900

    Merge branch 'master' of https://github.com/safiab/Think-Like-a-Scientist-Excersices
    # Conflicts:
    #	Chp1/Excersice1.4.py

--- Chp1/Excersice1.1.py
+++ Chp1/Excersice1.1.py
@@ -1,3 +1,3 @@
 # Exercise 1.1 Write a well-structured English sentence with invalid tokens in it.
 # Then write another sentence with all valid tokens but with invalid structure.
-#
+# The purpose of this exercise is to understand how the program is parsed.

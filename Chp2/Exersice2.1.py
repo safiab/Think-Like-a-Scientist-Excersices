a82db239e2c45295c39c0995c3fcff64e08c890a
safiab <safia.hirari@gmail.com>
2018-01-15 13:38:43 +0900

    added

---
+++ Chp2/Exersice2.1.py
@@ -0,0 +1,10 @@
+# Exercise 2.1
+# In the shell try the following:
+# >>> m = 1,000,000
+# >>> type(m)
+
+# or try this here
+m = 1, 000, 000
+print(type(m))
+
+# For now, notice that the type of the variable m is not an integer

c1f05b4864364669e821acf8297272fd55cb0af3
safiab <safia.hirari@gmail.com>
2018-01-15 12:36:40 +0900

    Update Excersice1.1.py

--- Chp1/Excersice1.1.py
+++ Chp1/Excersice1.1.py
@@ -1,2 +1,11 @@
-#Exercise 1.1 Write a well-structured English sentence with invalid tokens in it. Then write another
-#sentence with all valid tokens but with invalid structure.
+# Exercise 1.1 Write a well-structured English sentence with invalid tokens in it. Then write another
+# sentence with all valid tokens but with invalid structure. When you read a sentence in English or 
+# a statement in a formal language, you have to figure out what the structure of the sentence is. 
+# This process is called parsing.
+
+# Write a well-structured English sentence with invalid tokens in it
+# I wrote a progam
+# here the invalid token is 'progam'
+
+#write another sentence with all valid tokens but with invalid structure
+# I program  wrote 
